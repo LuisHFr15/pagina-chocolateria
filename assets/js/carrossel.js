cd3796cd6e57dd366df2b6f87ec2f4ffc2f067f1
Luís Henrique F Ramires <luishenriquefr@outlook.com>
2024-01-06 14:18:38 -0300

    made the carousel, but it doesn't works, must fix it

---
+++ assets/js/carrossel.js
@@ -0,0 +1,16 @@
+let slides = document.querySelectorAll('.slide-container')
+let i = 0;
+
+function next() {
+    slides[i].classList.remove('active');
+    i = (i + 1) % slides.length;
+    slides[i].classList.add('active');
+}
+
+function prev() {
+    slides[i].classList.remove('active');
+    i = (i - 1 + slides.length) % slides.length;
+    slides[i].classList.add('active');
+}
+
+setInterval(next, 7000);
